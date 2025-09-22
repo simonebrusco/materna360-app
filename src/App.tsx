4321c0a04597a06721457e5a56d3f7e2398a698f
simonebrusco <simonebrusco@gmail.com>
2025-09-22 08:22:43 -0300

    Create App.tsx

---
+++ src/App.tsx
@@ -0,0 +1,7 @@
+export default function App() {
+  return (
+    <div className="min-h-screen flex items-center justify-center p-8">
+      <h1 className="text-3xl font-bold">materna360 🚀</h1>
+    </div>
+  )
+}
