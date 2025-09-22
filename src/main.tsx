9acdb15892173a413414bd717a7755c71cd4e965
simonebrusco <simonebrusco@gmail.com>
2025-09-22 10:01:51 -0300

    Update main.tsx
    fix: import index.css from root

--- src/main.tsx
+++ src/main.tsx
@@ -1,7 +1,7 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App'
-import '../index.css'
+import '../index.css'   // <- IMPORT CORRIGIDO: sobe um nível para pegar index.css na raiz
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
