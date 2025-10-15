39b2c22289ba73fcbca6ddd53597212c2bd459eb
simonebrusco <simonebrusco@gmail.com>
2025-10-14 22:47:16 -0300

    Update next.config.js

--- materna360_starter/next.config.js
+++ materna360_starter/next.config.js
@@ -1,9 +1,12 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  reactStrictMode: true,
-  experimental: {
-    typedRoutes: false,
+  async redirects() {
+    return [
+      { source: '/activities', destination: '/brincar', permanent: true },
+      { source: '/activities/:slug*', destination: '/brincar/:slug*', permanent: true },
+      { source: '/wellbeing', destination: '/cuidar', permanent: true },
+      { source: '/profile', destination: '/eu360', permanent: true },
+    ];
   },
 };
-
 module.exports = nextConfig;
