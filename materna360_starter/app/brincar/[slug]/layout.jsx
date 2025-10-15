fdfaa827c81ad9ff31bd63449d71a574b0befb7b
simonebrusco <simonebrusco@gmail.com>
2025-10-15 05:11:11 -0300

    Create layout.jsx

---
+++ materna360_starter/app/brincar/[slug]/layout.jsx
@@ -0,0 +1,9 @@
+// Server Component
+export const metadata = {
+  title: "Brincar • Detalhe",
+  description: "Detalhes da atividade de brincar.",
+};
+
+export default function BrincarItemLayout({ children }) {
+  return children;
+}
