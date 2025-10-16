0929b622f80e43be94fe2d496eb11ca337d32e39
simonebrusco <simonebrusco@gmail.com>
2025-10-16 10:01:15 -0300

    Create page.jsx

---
+++ materna360_starter/app/meu-dia/momentos/page.jsx
@@ -0,0 +1,8 @@
+export default function MomentosPage(){
+  return (
+    <main className="pb-28">
+      <h1 className="title mb-3">Tempo com Meu Filho</h1>
+      <div className="card">Em construção: registrar momento + álbum.</div>
+    </main>
+  );
+}
