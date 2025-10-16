9edcba3546fb71f118817fe97ca1dade7190dfd8
simonebrusco <simonebrusco@gmail.com>
2025-10-16 11:06:51 -0300

    Create day.js

---
+++ materna360_starter/lib/day.js
@@ -0,0 +1,5 @@
+// lib/day.js
+export function todayKey() {
+  const d = new Date();
+  return `${d.getFullYear()}-${String(d.getMonth()+1).padStart(2,"0")}-${String(d.getDate()).padStart(2,"0")}`;
+}
